e9a965d2346e4001c095ba942342a50e4a381c80
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-11-08 20:34:09 -0300

    Implements input masking and validates the phone number input field

--- clipz/src/app/shared/shared.module.ts
+++ clipz/src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { TabsContainerComponent } from './tabs-container/tabs-container.componen
 import { TabComponent } from './tab/tab.component';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { provideEnvironmentNgxMask, NgxMaskDirective } from 'ngx-mask';
 // import { ModalService } from '../services/modal.service';
 
 
@@ -18,7 +19,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxMaskDirective
   ],
   exports: [
     ModalComponent,
@@ -26,6 +28,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     TabComponent,
     InputComponent
   ],
+  providers: [
+    provideEnvironmentNgxMask()
+  ]
   // providers: [
   //   ModalService
   // ]
