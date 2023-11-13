1c73a6f792bbe60be730f465a5184eb36685029b
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-11-13 11:30:47 -0300

    Integrates firebase sdk

---
+++ clipz/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyAYnnGKEBiLTrdL9LUd6nytDxEZYnO6Vlw",
+        authDomain: "clipz-44620.firebaseapp.com",
+        projectId: "clipz-44620",
+        storageBucket: "clipz-44620.appspot.com",
+        appId: "1:1045019684330:web:59d291fec80e2f38aa1693"
+      }
+}
---
+++ clipz/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyAYnnGKEBiLTrdL9LUd6nytDxEZYnO6Vlw",
+        authDomain: "clipz-44620.firebaseapp.com",
+        projectId: "clipz-44620",
+        storageBucket: "clipz-44620.appspot.com",
+        messagingSenderId: "1045019684330",
+        appId: "1:1045019684330:web:59d291fec80e2f38aa1693"
+      }
+};
