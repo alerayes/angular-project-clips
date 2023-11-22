8f7831a607550584baa1745410a94b64682bd38e
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-11-21 23:00:17 -0300

    Handles the multi step form

--- clipz/src/app/video/upload/upload.component.ts
+++ clipz/src/app/video/upload/upload.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class UploadComponent implements OnInit{
   isDragover = false
   file: File | null = null
+  nextStep = false
 
   constructor() {}
 
@@ -24,6 +25,6 @@ export class UploadComponent implements OnInit{
       return
     }
 
-    console.log(this.file)
+    this.nextStep = true
   }
 }
