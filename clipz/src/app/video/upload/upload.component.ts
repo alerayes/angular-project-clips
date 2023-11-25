35f7de9c7700ed366eeaae0a3eaf21737c8ceaab
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-11-25 19:58:09 -0300

    Handles errors and successful uploads

--- clipz/src/app/video/upload/upload.component.ts
+++ clipz/src/app/video/upload/upload.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid'
+import { last } from 'rxjs/operators'
 
 @Component({
   selector: 'app-upload',
@@ -17,6 +18,7 @@ export class UploadComponent implements OnInit{
   alertMsg = 'Please wait! Your clip is being uploaded.'
   inSubmission = false
   percentage = 0
+  showPercentage = false
 
   title = new FormControl('',{
     validators: [
@@ -58,6 +60,7 @@ export class UploadComponent implements OnInit{
     this.alertColor = 'blue'
     this.alertMsg = 'Please wait! Your clip is being uploaded.'
     this.inSubmission = true
+    this.showPercentage = true
 
     const clipFileName = uuid()
     const clipPath = `clips/${clipFileName}.mp4`
@@ -66,6 +69,23 @@ export class UploadComponent implements OnInit{
 
     task.percentageChanges().subscribe(progress => {
       this.percentage = progress as number / 100
+    })
+    
+    task.snapshotChanges().pipe(
+      last()
+    ).subscribe({
+      next: (snapshot) => {
+        this.alertColor = 'green'
+        this.alertMsg = 'Success! Your clip is now ready to share with the world.'
+        this.showPercentage = false
+      },
+      error: (error) => {
+        this.alertColor = 'red'
+        this.alertMsg = 'Upload failed! Please try again later.'
+        this.inSubmission = true
+        this.showPercentage = false
+        console.error(error)
+      }
     })
   }
 }
