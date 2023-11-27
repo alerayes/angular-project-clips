a7fec5b565fffc06a5fc254905a4bd056f8943af
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-11-27 18:28:13 -0300

    Queries the database

--- clipz/src/app/services/clip.service.ts
+++ clipz/src/app/services/clip.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, DocumentReference } from '@angular/fire/compat/firestore'
 import IClip from '../models/clip.model';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +12,8 @@ export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>
 
   constructor(
-    private db: AngularFirestore
+    private db: AngularFirestore,
+    private auth: AngularFireAuth
   ) { 
     this.clipsCollection = db.collection('clips')
   }
@@ -17,4 +21,20 @@ export class ClipService {
 createClip(data: IClip) : Promise<DocumentReference<IClip>> {
     return this.clipsCollection.add(data)
   }
+
+public getUserClips() {
+  return this.auth.user.pipe(
+    switchMap(user => {
+      if(!user) {
+        return of([])
+      }
+
+      const query = this.clipsCollection.ref.where(
+        'uid', '==', user.uid
+      )
+
+      return query.get()
+    })
+  )
+}
 }
