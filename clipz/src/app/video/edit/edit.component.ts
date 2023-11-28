81286a7b4d96c9fea95929349d125543b8d35c8b
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-11-28 11:10:33 -0300

    Binds the edit form

--- clipz/src/app/video/edit/edit.component.ts
+++ clipz/src/app/video/edit/edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/models/clip.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -9,6 +10,21 @@ import IClip from 'src/app/models/clip.model';
 })
 export class EditComponent implements OnInit, OnDestroy{
   @Input() activeClip: IClip | null = null
+
+  clipID = new FormControl('', {
+    nonNullable: true
+  })
+  title = new FormControl('', {
+    validators: [
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable: true
+  })
+  editForm = new FormGroup({
+    title: this.title,
+    id: this.clipID
+  })
 
   constructor(
     private modal: ModalService
@@ -21,4 +37,14 @@ export class EditComponent implements OnInit, OnDestroy{
   ngOnDestroy(): void {
     this.modal.unregister('editClip')
   }
+
+  ngOnChanges(): void {
+    if(!this.activeClip){
+      return
+    }
+
+    this.clipID.setValue(this.activeClip.docID as string)
+    this.title.setValue(this.activeClip.title)
+  }
+
 }
