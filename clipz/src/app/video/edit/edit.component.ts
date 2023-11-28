d2b9b518c1d6d89515930f8d8bfa870854fb3a4a
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-11-28 11:43:43 -0300

    Updates the list of clips in the manage page

--- clipz/src/app/video/edit/edit.component.ts
+++ clipz/src/app/video/edit/edit.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit, OnDestroy, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, 
+         OnInit, 
+         OnDestroy, 
+         Input, 
+         OnChanges, 
+         Output,
+         EventEmitter } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/models/clip.model';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ClipService } from 'src/app/services/clip.service';
+
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
-export class EditComponent implements OnInit, OnDestroy{
+export class EditComponent implements OnInit, OnDestroy, OnChanges{
   @Input() activeClip: IClip | null = null
   inSubmission = false
   showAlert = false
   alertColor = 'blue'
   alertMsg = 'Please wait! Updating clip.'
+  @Output() update = new EventEmitter()
 
   clipID = new FormControl('', {
     nonNullable: true
@@ -49,11 +57,17 @@ export class EditComponent implements OnInit, OnDestroy{
       return
     }
 
+    this.inSubmission = false
+    this.showAlert = false
     this.clipID.setValue(this.activeClip.docID as string)
     this.title.setValue(this.activeClip.title)
   }
 
   async submit(){
+    if(!this.activeClip) {
+      return
+    }
+
     this.inSubmission = true
     this.showAlert = true
     this.alertColor = 'blue'
@@ -70,6 +84,9 @@ export class EditComponent implements OnInit, OnDestroy{
       this.alertMsg = 'Something went wrong. Try again later.'
       return
     }
+
+    this.activeClip.title = this.title.value
+    this.update.emit(this.activeClip)
 
     this.inSubmission = false
     this.alertColor = 'green'
